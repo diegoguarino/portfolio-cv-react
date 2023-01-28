63ac674139fb70e33182c5371f4a59494d4c6e7e
diegoguarino <diegoguarino.dev@gmail.com>
2023-01-28 04:47:19 -0300

    cambios

--- src/components/Frontend.jsx
+++ src/components/Frontend.jsx
@@ -5,45 +5,48 @@ export const Frontend = () => {
             <h3 className="skills_title">Frontend Developer</h3>
             <div className="skills_box">
                 <div className="skills_group">
-                        <div className="skills_data">
-                        <i className='bx bxl-html5'></i>
 
+                    <div className="skills_data">
+                        <i className='bx bxl-html5'></i>
                         <div>
                             <h5 className="skills_name">HTML</h5>
                         </div>
-                        </div>
+                    </div>
 
-                        <div className="skills_data">
+                    <div className="skills_data">
                         <i className='bx bxl-css3'></i>
-
                         <div>
                              <h5 className="skills_name">CSS</h5>
                         </div>
-                        </div>
+                    </div>
 
-                        <div className="skills_data">
+                    <div className="skills_data">
                          <i className='bx bxl-javascript'></i>
-
                          <div>
                              <h5 className="skills_name">Javascript</h5>
                          </div>
-                        </div>
+                    </div>
 
-                        <div className="skills_data">
+                    <div className="skills_data">
                         <i className='bx bxl-bootstrap'></i>
-
                         <div>
                             <h5 className="skills_name">Bootstrap</h5>
                         </div>
-                        </div>
+                    </div>
 
-                        <div className="skills_data">
+                    <div className="skills_data">
                         <i className='bx bxl-react'></i>
                         <div>
                            <h5 className="skills_name">React JS</h5>
                         </div>
-                        </div>
+                    </div>
 
+                    <div className="skills_data">
+                        <i className='bx bxl-redux'></i>
+                        <div>
+                           <h5 className="skills_name">Redux</h5>
+                        </div>
+                    </div>
 
                 </div>
             </div>
